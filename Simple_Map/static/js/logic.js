c37dbb62c007077396b832551ab7287db385d145
unknown <thi.nguyen2307@gmail.com>
2020-01-08 15:36:52 -0800

    Add file

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,11 +1,11 @@
-// Add map view
+// // Create the map object with a center and zoom level.
 var map = L.map('mapid', {
     center: [39.5, -98.5],
     zoom: 3,
 });
 
-// Add tile
-let tile_url = 'https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}';
+// Add tile layer that will be the background of our map.
+let tile_url = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}';
 let streets = L.tileLayer(tile_url, {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
