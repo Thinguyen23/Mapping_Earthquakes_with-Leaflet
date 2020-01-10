2bea3f5ad1c0c3f38d07cae063b6341e5736f12c
unknown <thi.nguyen2307@gmail.com>
2020-01-09 18:34:29 -0800

    Add files to Mapping_Single_Points

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,14 @@
+// // Create the map object with a center and zoom level.
+var map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+});
+
+// Add tile layer that will be the background of our map.
+let tile_url = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}';
+let streets = L.tileLayer(tile_url, {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+})
+    streets.addTo(map);
